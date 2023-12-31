6f8eb1fc0b9b1f03c829812b71159e705745c339
一闪一闪亮晶晶 <resetsix223@gmail.com>
2023-12-30 21:56:01 -0500

    fix: fix activeRule url

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BasePage } from "./pages/BasePage";
-import { useRoutes } from "react-router-dom";
+import { Navigate, useRoutes } from "react-router-dom";
 import "./App.css";
 
 const routes = [
 	{
-		path: "/*", // 路由
+		path: "/", // 路由
+		element: <Navigate to="/qiankun-demo" />, // 路由对应页面组件
+	},
+	{
+		path: "/qiankun-demo/*", // 路由
 		element: <BasePage />, // 路由对应页面组件
 	},
 ];
